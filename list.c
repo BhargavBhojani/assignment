874df4f05f82978c7dc1c72f956f57cf2ff622dd
BhargavBhojani <32099587+BhargavBhojani@users.noreply.github.com>
2022-04-11 00:15:35 -0400

    Update list.c

--- list.c
+++ list.c
@@ -1,148 +1,83 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
+//https://github.com/hardik05/Damn_Vulnerable_C_Program/blob/master/imgRead.c
 
-#define FLAG_BUFFER 128
-#define MAX_SYM_LEN 4
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-typedef struct Stonks {
-	int shares;
-	char symbol[MAX_SYM_LEN + 1];
-	struct Stonks *next;
-} Stonk;
-
-typedef struct Portfolios {
-	int money;
-	Stonk *head;
-} Portfolio;
-
-int view_portfolio(Portfolio *p) {
-	if (!p) {
-		return 1;
-	}
-	printf("\nPortfolio as of ");
-	fflush(stdout);
-	system("date"); // TODO: implement this in C
-	fflush(stdout);
-
-	printf("\n\n");
-	Stonk *head = p->head;
-	if (!head) {
-		printf("You don't own any stonks!\n");
-	}
-	while (head) {
-		printf("%d shares of %s\n", head->shares, head->symbol);
-		head = head->next;
-	}
-	return 0;
-}
-
-Stonk *pick_symbol_with_AI(int shares) {
-	if (shares < 1) {
-		return NULL;
-	}
-	Stonk *stonk = malloc(sizeof(Stonk));
-	stonk->shares = shares;
-
-	int AI_symbol_len = (rand() % MAX_SYM_LEN) + 1;
-	for (int i = 0; i <= MAX_SYM_LEN; i++) {
-		if (i < AI_symbol_len) {
-			stonk->symbol[i] = 'A' + (rand() % 26);
-		} else {
-			stonk->symbol[i] = '\0';
-		}
-	}
-
-	stonk->next = NULL;
-
-	return stonk;
-}
-
-int buy_stonks(Portfolio *p) {
-	if (!p) {
-		return 1;
-	}
-	char api_buf[FLAG_BUFFER];
-	FILE *f = fopen("api","r");
-	if (!f) {
-		printf("Flag file not found. Contact an admin.\n");
-		exit(1);
-	}
-	fgets(api_buf, FLAG_BUFFER, f);
-
-	int money = p->money;
-	int shares = 0;
-	Stonk *temp = NULL;
-	printf("Using patented AI algorithms to buy stonks\n");
-	while (money > 0) {
-		shares = (rand() % money) + 1;
-		temp = pick_symbol_with_AI(shares);
-		temp->next = p->head;
-		p->head = temp;
-		money -= shares;
-	}
-	printf("Stonks chosen\n");
-
-	// TODO: Figure out how to read token from file, for now just ask
-
-	char *user_buf = malloc(300 + 1);
-	printf("What is your API token?\n");
-	scanf("%300s", user_buf);
-	printf("Buying stonks with token:\n");
-	printf(user_buf);
-
-	// TODO: Actually use key to interact with API
-
-	view_portfolio(p);
-
-	return 0;
-}
-
-Portfolio *initialize_portfolio() {
-	Portfolio *p = malloc(sizeof(Portfolio));
-	p->money = (rand() % 2018) + 1;
-	p->head = NULL;
-	return p;
-}
-
-void free_portfolio(Portfolio *p) {
-	Stonk *current = p->head;
-	Stonk *next = NULL;
-	while (current) {
-		next = current->next;
-		free(current);
-		current = next;
-	}
-	free(p);
-}
-
-int main(int argc, char *argv[])
+struct Image
 {
-	setbuf(stdout, NULL);
-	srand(time(NULL));
-	Portfolio *p = initialize_portfolio();
-	if (!p) {
-		printf("Memory failure\n");
-		exit(1);
+	char header[4];
+	int width;
+	int height;
+	char data[10];
+};
+
+int ProcessImage(char* filename){
+
+	FILE *fp;
+	char ch;
+	struct Image img;
+
+	fp = fopen(filename,"r"); 
+
+	if(fp == NULL)
+	{
+		printf("\nCan't open file or file doesn't exist.");
+		exit(0);
 	}
 
-	int resp = 0;
+	printf("\n\tHeader\twidth\theight\tdata\t\r\n");
 
-	printf("Welcome back to the trading app!\n\n");
-	printf("What would you like to do?\n");
-	printf("1) Buy some stonks!\n");
-	printf("2) View my portfolio\n");
-	scanf("%d", &resp);
+	while(fread(&img,sizeof(img),1,fp)>0){
+		printf("\n\t%s\t%d\t%d\t%s\r\n",img.header,img.width,img.height,img.data);
+	
+		int size1 = img.width + img.height; //Vulnerability: integer overflow
+		char* buff1=(char*)malloc(size1);
 
-	if (resp == 1) {
-		buy_stonks(p);
-	} else if (resp == 2) {
-		view_portfolio(p);
+		memcpy(buff1,img.data,sizeof(img.data)); //Vulnerability: no data buffer size/malloc success check?
+		free(buff1);
+	
+		if (size1/2==0){
+			free(buff1); //Vulnerability: double free
+		}
+		else{
+			if(size1 == 123456){
+				buff1[0]='a'; //Vulnerability: use after free
+			}
+		}
+
+		int size2 = img.width - img.height+100; //Vulnerability: integer underflow
+		//printf("Size1:%d",size1);
+		char* buff2=(char*)malloc(size2);
+
+		memcpy(buff2,img.data,sizeof(img.data));
+
+		int size3= img.width/img.height;
+		//printf("Size2:%d",size3);
+
+		char buff3[10];
+		char* buff4 =(char*)malloc(size3);
+		memcpy(buff4,img.data,sizeof(img.data));
+
+		char OOBR_stack = buff3[size3+100]; //Vulnerability: out of bound read (stack)
+		char OOBR_heap = buff4[100];
+
+		buff3[size3+100]='c'; //Vulnerability: out of bound write (Stack)
+		buff4[100]='c'; //Vulnerability: out of bound write (Heap)
+
+		if(size3>10){
+				buff4=0; //memory leak?
+		}
+		else{
+			free(buff4);
+		}
+
+		free(buff2);
 	}
+	fclose(fp);
+}
 
-	free_portfolio(p);
-	printf("Goodbye!\n");
-
-	exit(0);
+int main(int argc,char **argv)
+{
+	ProcessImage(argv[1]);
 }
